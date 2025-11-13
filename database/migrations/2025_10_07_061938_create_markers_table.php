5d3e3a9eb8bae2e2aafc9ce18e82f211ca91f86a
zin-00 <meljun.mariquit26@gmail.com>
2025-11-13 15:48:24 +0800

    Refactor markers table migration to remove unnecessary change calls for latitude and longitude columns

--- database/migrations/2025_10_07_061938_create_markers_table.php
+++ database/migrations/2025_10_07_061938_create_markers_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
         {
             Schema::create('markers', function (Blueprint $table) {
                 $table->id();
-                $table->decimal('latitude', 10, 8)->change();
-                $table->decimal('longitude', 11, 8)->change();
+                $table->decimal('latitude', 10, 8);
+                $table->decimal('longitude', 11, 8);
                 $table->string('label');
                 $table->string('type')->default('building');
                 $table->timestamps();
